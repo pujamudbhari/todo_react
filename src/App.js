f5db4bc2003315cbb067dd2fdfe108a23acef3c6
pujamudbhari <pujamudbhari1234@gmail.com>
2021-12-02 19:08:04 +0545

    done with basic layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
-      <h1>PUJA's TODO LIST</h1>
+        <h1>PUJA's TODO LIST</h1>
       </header>
+      <Form todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      <TodoList />
     </div>
   );
 }
